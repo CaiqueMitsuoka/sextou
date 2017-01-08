5bd12c6d3f656d04174a116f50f3af9e530b7396
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-01-08 17:59:04 -0200

    Major refactor to controllers and DRY improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def current_user
+    @user ||= RSpotify::User.new(session[:user_hash])
+  end
+
+  def current_playlist
+    @playlist ||= RSpotify::Playlist.find(current_user.id , session[:current_playlist])
+  end
 end

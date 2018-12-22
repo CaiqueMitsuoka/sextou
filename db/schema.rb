900a5122f1038767da123e42fce53141b0af7c69
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2018-12-22 18:57:35 -0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20170101153159) do
   create_table "users", force: :cascade do |t|
     t.string   "name",       null: false
     t.string   "user_raw"
-    t.boolean  "online",     null: false
     t.string   "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

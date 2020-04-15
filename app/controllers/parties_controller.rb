db22c704f69c94fca57c134d823c3aa35304ca47
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-04-14 22:50:29 -0300

    Update party views

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,31 +2,31 @@ class PartiesController < ApplicationController
 
   # GET /parties
   def index
-    @parties = Party.all
+    @parties = current_user.parties
   end
 
   # GET /parties/new
   def new
-    @party = Party.new
+    @party = Party.new()
   end
 
   # POST /parties
   def create
     @party = Party.new(party_params)
     playlist = Playlist::Create.new(@party.name, current_user).execute
-    @party.playlist_id = playlist.id
 
-    respond_to do |format|
-      if @party.save
-        redirect_to @party, notice: 'Party was successfully created.'
-      else
-        render :new
-      end
+    @party.playlist_id = playlist.id
+    @party.user = current_user
+
+    if @party.save
+      redirect_to @party, notice: 'Party was successfully created.'
+    else
+      render :new
     end
   end
 
   private
     def party_params
-      params.require(:party).permit(:name, :slug, :user_id)
+      params.require(:party).permit(:name)
     end
 end

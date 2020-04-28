810f5b494c9068ed1c3855488c4ea8287b1d26b4
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-04-28 13:17:14 -0300

    Add create releases playlist first draft

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'],ENV['SPOTIFY_SECRET_ID'], scope: 'user-read-email playlist-modify-public playlist-modify-private user-library-read user-library-modify'
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'],ENV['SPOTIFY_SECRET_ID'], scope: 'user-read-email playlist-modify-public playlist-modify-private user-library-read user-library-modify user-follow-read'
 end
 
 module OmniAuth

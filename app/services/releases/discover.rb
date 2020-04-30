54af42d6df0a8740c7bde1d7d2f6fc53cff457ae
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-04-30 02:41:19 -0300

    Refactor to group releases

--- app/services/releases/discover.rb
+++ app/services/releases/discover.rb
@@ -7,30 +7,54 @@ module Releases
     end
 
     def execute
+      all_releases = []
       playlist = ::Playlist::Create.new("🔥Sextou lançamentos🎉", user).execute
-      date_limit = 7.days.ago.beginning_of_day
 
-      artists.each do |artist|
-        releases = albums(artist)
+      artists.each {|artist| all_releases << albums(artist)}
+      new_releases = all_releases.flatten.select {|album| is_new?(album)}
 
-        new_releases = releases.select do |album|
-          if album.release_date_precision == "day"
-            DateTime.iso8601(album.release_date) > date_limit
-          else
-            false
-          end
-        end
-
-        new_releases.each do |album|
-          puts "Adicionando #{album.name} de #{album.release_date}"
-          playlist.add_tracks!(album.tracks)
-        end
+      new_releases.each do |album|
+        puts "Adicionando #{album.name} de #{album.release_date}"
+        playlist.add_tracks!(album.tracks)
       end
     end
 
     private
 
     def artists
+      @artists ||= fetch_artists
+    end
+
+    def is_new?(album)
+      if album.release_date_precision == "day"
+        DateTime.iso8601(album.release_date) > date_limit
+      else
+        false
+      end
+    end
+
+    def albums(artist)
+      albums = []
+      offset = 0
+
+      (1..50).detect do |page_number|
+        page_albums = artist.albums(limit: 50, offset: offset, include_groups: 'album,single')
+
+        offset += 50
+
+        albums.concat(page_albums)
+
+        page_albums.count < 50
+      end
+
+      albums.sort {|a,b| a.release_date <=> b.release_date}.reverse
+    end
+
+    def date_limit
+      @date_limit ||= 7.days.ago.beginning_of_day
+    end
+
+    def fetch_artists
       artists = []
       last_artist_id = nil
 
@@ -44,23 +68,6 @@ module Releases
       end
 
       artists
-    end
-
-    def albums(artist)
-      albums = []
-      offset = 0
-
-      (1..50).detect do |page_number|
-        page_albums = artist.albums(limit: 50, offset: offset)
-
-        offset += 50
-
-        albums.concat(page_albums)
-
-        page_albums.count < 50
-      end
-
-      albums.sort {|a,b| a.release_date <=> b.release_date}.reverse
     end
   end
 end

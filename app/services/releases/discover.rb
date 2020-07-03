d02a872cd664b9b4669b3a93d0078b8a509b2aaa
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-07-03 19:30:58 -0300

    Update Spotify Request retry

--- app/services/releases/discover.rb
+++ app/services/releases/discover.rb
@@ -84,7 +84,7 @@ module Releases
       begin
         yield
       rescue RestClient::TooManyRequests => e
-        wait_time = e.http_headers[:retry_after]
+        wait_time = e.http_headers[:retry_after].to_i
         wait_time ||= 10
 
         sleep wait_time.seconds

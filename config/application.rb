8bc96f4dc2dbcdd3d1fc4344c0b194aea2fd4a98
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-07-06 00:27:07 -0300

    Add sidekiq

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,6 @@ module Sextou
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.active_job.queue_adapter = :sidekiq
   end
 end

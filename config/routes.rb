e4f1e540df0360f5d6452e72a424db933888dc8f
Caique Hitoshi Mitsuoka <caique.mitsuoka@gmail.com>
2020-07-25 19:03:35 -0300

    Add sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   get '/', to: 'spotify#home'
   get '/auth/spotify/callback', to: 'spotify#spotify'
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
     get '/:slug/tracks', to: 'tracks#index', as: :tracks
     post '/:slug/tracks/', to: 'tracks#create'
   end
+
+  mount Sidekiq::Web => '/sidekiq', constraints: UserIsAdmin
 
   resources :friday_releases, only: [:new, :create, :destroy]
   resources :snapshot, only: [:index, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_053849) do
+ActiveRecord::Schema.define(version: 2020_07_25_215746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_053849) do
     t.string "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "parties", "users"
